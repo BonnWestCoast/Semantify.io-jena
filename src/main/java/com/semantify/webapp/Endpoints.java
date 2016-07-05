496614dc347b480a743199ddf40d3b4c113fec5c
Omar Gutiérrez <omar.vpa@gmail.com>
2016-07-06 01:29:49 +0200

    Refactored access to database

--- src/main/java/com/semantify/webapp/Endpoints.java
+++ src/main/java/com/semantify/webapp/Endpoints.java
@@ -2,15 +2,15 @@ package com.semantify.webapp;
 
 import javax.ws.rs.*;
 import java.io.*;
-import java.sql.*;
 import java.util.ArrayList;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.MediaType;
 
 import com.google.gson.Gson;
-import org.apache.commons.io.IOUtils;
 import org.glassfish.jersey.media.multipart.FormDataParam;
 import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
+import opcua.ontmalizer.XML2OWL;
+import opcua.ontmalizer.XSD2OWL;
 
 /**
  * This is a POJO class useful to return JSON format in the response of the services.
@@ -42,9 +42,7 @@ class Element {
 class elementList {
 
     public ArrayList<Element> list;
-
     public elementList () {}
-
     public elementList(ArrayList<Element> list) {
         this.list = list;
     }
@@ -63,7 +61,12 @@ public class Endpoints {
     ) {
 
         String fileLocation = fileName.getFileName();
-        saveToDisk(uploadedInputStream, fileName);
+        DBController objectController = new DBController();
+        objectController.storeFile(uploadedInputStream, fileName);
+
+        //XSD2OWL conversor = new XSD2OWL();
+        //conversor.createOPCUAOntology();
+
         return Response.status(200).build();
     }
 
@@ -73,38 +76,12 @@ public class Endpoints {
     @Produces(MediaType.APPLICATION_JSON)
     public Response getMessage() {
 
-        Connection conn = null;
-        Statement stmt = null;
-        ArrayList<Element> list = new ArrayList<Element>();
-
-        try {
-
-            Class.forName("org.sqlite.JDBC");
-            conn = DriverManager.getConnection("jdbc:sqlite:files.db");
-            stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery("select * from files");
-
-            while (rs.next()) {
-                int id = rs.getInt("id");
-                String name = rs.getString("name");
-                Element element = new Element(id, name);
-                list.add(element);
-            }
-
-            stmt.close();
-            conn.close();
-
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
+        DBController query = new DBController();
+        ArrayList<Element> list = query.listElements();
         String json = new Gson().toJson(new elementList(list));
-
         return Response.status(200).entity(json).build();
-    }
 
+    }
 
     @GET
     @Path("/load/{id}")
@@ -113,100 +90,12 @@ public class Endpoints {
     public Response getMessage(
             @PathParam("id") String id
     ) {
-
-        Connection conn = null;
-        PreparedStatement stmt = null;
-        Element element = null;
-
-        try {
-
-            Class.forName("org.sqlite.JDBC");
-            conn = DriverManager.getConnection("jdbc:sqlite:files.db");
-            stmt = conn.prepareStatement("select * from files where id = ?");
-            stmt.setString(1, id);
-            ResultSet rs = stmt.executeQuery();
-
-            while (rs.next()) {
-                int id_ = rs.getInt("id");
-                String name = rs.getString("name");
-                String content = rs.getString("xml_file");
-                element = new Element(id_, name, content);
-            }
-
-            stmt.close();
-            conn.close();
-
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
+        DBController query = new DBController();
+        Element element = new Element();
         String json = new Gson().toJson(element);
         return Response.status(200).entity(json).build();
     }
 
-    private void saveToDisk(InputStream is, FormDataContentDisposition fileName) {
 
-        String fileLocation = "/home/hariseldon/" + fileName.getFileName();
 
-        try {
-            OutputStream out = new FileOutputStream(new File(fileLocation));
-            IOUtils.copy(is, out);
-            is.close();
-            out.close();
-        } catch (IOException e) {
-            System.out.println("Exception");
-            e.printStackTrace();
-        }
-
-        SQLStuff(fileName.getFileName(), fileLocation);
-
-    }
-
-    Connection conn = null;
-    Statement stmt = null;
-
-    public void SQLStuff(String fileName, String fileLocation){
-        try {
-
-            Class.forName("org.sqlite.JDBC");
-            conn = DriverManager.getConnection("jdbc:sqlite:files.db");
-
-            if (conn != null) {
-
-                /* create table if not exists */
-                stmt = conn.createStatement();
-                String sql = " create table if not exists files ( " +
-                             " id integer primary key autoincrement, " +
-                             " name text not null, " +
-                             " xml_file blob " +
-                             ")";
-                stmt.executeUpdate(sql);
-
-                /* insert new instance */
-                sql = "insert into files (name, xml_file) values (?, ?)";
-                PreparedStatement stmt = conn.prepareStatement(sql);
-                stmt.setString(1, fileName);
-
-                File xmlFile = new File(fileLocation);
-                FileInputStream fis = new FileInputStream(xmlFile);
-                stmt.setBinaryStream(2, fis, (int) xmlFile.length());
-                stmt.execute();
-
-                stmt.close();
-                conn.close();
-                System.out.println("New instance");
-
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
 }
-
