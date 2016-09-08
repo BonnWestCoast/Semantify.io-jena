a33a6e0820950e165da937b093d0926a30b678d5
cherurg <karpovad@yandex.ru>
2016-09-08 14:10:01 +0300

    for dev

---
+++ js/app.js
@@ -0,0 +1,55 @@
+
+/* Little script to communicate Node.js with an API Rest */
+
+var request = require('request');
+var fs = require('fs');
+var url = 'http://localhost:8080/rest/instance/new'
+
+req = request.post(
+        url,
+        function (error, response, body) {
+
+            if (!error && response.statusCode == 200) {
+                console.log('ok')
+            } else {
+                console.log(response);
+                console.log(response.statusCode);
+                console.log('error')
+            }
+        }
+)
+
+var form = req.form();
+form.append('file', fs.createReadStream('./example.xml'));
+
+var url = 'http://localhost:8080/rest/instance/list'
+req = request.get(
+        url,
+        function (error, response, body) {
+
+            if (!error && response.statusCode == 200) {
+                console.log(body);
+                console.log('ok')
+            } else {
+                console.log(response);
+                console.log(response.statusCode);
+                console.log('error')
+            }
+        }
+)
+
+var url = 'http://localhost:8080/rest/instance/load/2'
+req = request.get(
+        url,
+        function (error, response, body) {
+
+            if (!error && response.statusCode == 200) {
+                console.log(body);
+                console.log('ok')
+            } else {
+                console.log(response);
+                console.log(response.statusCode);
+                console.log('error')
+            }
+        }
+)
